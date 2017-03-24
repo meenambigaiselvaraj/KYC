50bfa00285bbbc72455d1566e4b0baa7210a2e00
meenambigaiselvaraj <meenambigaiselvaraj@gmail.com>
2017-03-24 16:49:40 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,85 +1,147 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Link, browserHistory } from 'react-router'
+//import { Link, browserHistory } from 'react-router'
 import Header from '../header/Header';
-import { Button, Textfield ,Grid, Cell} from 'react-mdl';
+import { Button, Textfield, Grid, Cell, Dialog, DialogTitle } from 'react-mdl';
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      aadhar: "",
+      pan: "",
       registerSuccess: false
     };
   }
 
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleOpenDialog() {
+    this.setState({
+      openDialog: true
+    });
+  }
+
+  handleCloseDialog() {
+    this.setState({
+      openDialog: false
+    });
+  }
+
   page() {
-     
-    return;
 
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var aadhar = document.getElementById('aadhar').value;
-    var pan = document.getElementById('pan').value;
+    console.log(this.state.name);
+    console.log(this.state.email);
+    console.log(this.state.phone);
+    console.log(this.state.aadhar);
+    console.log(this.state.pan);
 
-    if (name === "") {
+    if (this.state.name === "") {
       alert('Enter your name');
       return false;
     }
 
-     if ((name.length < 3) || (name.length > 20)) {
+    if (((this.state.name).length < 3) || ((this.state.name).length > 20)) {
       alert("Name must be 3 to 20 Character");
       return
     }
 
-     if (!isNaN(name)) {
-      alert("Please Enter Only Characters");
-      return
-    }
-
-       var emailfilter = /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i
-    var b = emailfilter.test(email);
+    var emailfilter = /^\w+[\+\.\w-]*@([\w-]+\.)*\w+[\w-]*\.([a-z]{2,4}|\d+)$/i
+    var b = emailfilter.test(this.state.email);
     if (b === false) {
       alert("Please Enter a valid Mail ID");
       return
     }
 
-    if ((phone === "") && ((phone.length < 10) || (phone.length < 10))) {
-      alert("please Enter a Phone Number with 10 Digit (Like : 9566137117) ");
+    if ((this.state.phone).length === "")  {
+      <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>Phone Number Should Not Be Empty)</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+    if((this.state.phone).length > 10 ){
+      <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>please Enter a Phone Number with 10 Digit (Like : 9566137789</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+     if((this.state.phone).length < 10 ){
+       <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>please Enter a Phone Number with 10 Digit (Like : 9566137789)</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+    if ((this.state.aadhar).length === "")  {
+      <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>Aadhar Number Should Not Be Empty</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+    if((this.state.aadhar).length > 12 ){
+      <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>please Enter a Aadhar Number with 12 Digit (Like : 956613237789)</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+     if((this.state.aadhar).length < 12 ){
+       <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>please Enter a Aadhar Number with 10 Digit (Like : 956612337789)</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+    if ((this.state.pan).length === "")  {
+      <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>Pan Number Should Not Be Empty</DialogTitle>
+        </Dialog>
+      </div>
+      return
+    }
+    if((this.state.pan).length > 10 ){
+     <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>please Enter a Pan Number with 10 Digit (Like : 9566137789)</DialogTitle>
+        </Dialog>
+      </div>
+  
+    }
+     if((this.state.pan).length < 10 ){
+       <div> <Button colored onClick={this.handleOpenDialog} raised ripple>Show Dialog</Button>
+        <Dialog open={this.state.openDialog}>
+          <DialogTitle>please Enter a Pan Number with 10 Digit (Like : 9566137789)</DialogTitle>
+        </Dialog>
+      </div>
       return
     }
 
-     if (isNaN(phone)) {
-      alert("Enter the valid Phone Number (Like : 9566137117) ");
-      return
-    }
-
-    if ((pan === "") && ((pan.length < 10) || (pan.length < 10))) {
-      alert("please Enter a Pan Number with 10 Digit (Like : 9566137117) ");
-      return
-    }
-
-    else if (isNaN(pan)) {
-      alert("Enter the valid Pan Number (Like : 9566137117) ");
-      return
-    }
-
-    if ((aadhar === "") && ((aadhar.length < 12) || (aadhar.length < 12))) {
-      alert("please Enter a Aadhar Number with 12 Digit (Like : 956613711789) ");
-      return
-    }
-
-     if (isNaN(aadhar)) {
-      alert("Enter the valid Aadhar Number (Like : 956613711789) ");
-      return
-    }
+    
 
     let getUserData = {
-      "name": name,
-      "email": email,
-      "phone": phone,
-      "aadhar": aadhar,
-      "pan": pan
+      "name": this.state.name,
+      "email": this.state.email,
+      "phone": this.state.phone,
+      "aadhar": this.state.aadhar,
+      "pan": this.state.pan,
+     
     };
     this.setState({ userData: getUserData });
     this.setState({ registerSuccess: true });
@@ -87,77 +149,86 @@ class App extends Component {
     // browserHistory.push("Header/" + name + "/" + email + "/" + phone + "/" + aadhar + "/" + pan);
   }
 
-  sign() {
-    console.log('signed up')
-  }
-  setData() {
-    var name = document.getElementById('name');
-    var email = document.getElementById('email');
-    var phone = document.getElementById('phone');
-    var aadhar = document.getElementById('aadhar');
-    var pan = document.getElementById('pan');
-
-    this.setState({ name, email, phone, aadhar, pan })
-  }
-
-  
-
   render() {
     if (this.state.registerSuccess === true) {
       return <Header userData={this.state.userData} />
     }
+
     return (
+
       <div className="appBody" >
-           <div className="appHeader">
-            <label > APPLICATION FORM </label> <br/>
-            </div>
-            <div className="appAlign" >
-              <Grid >
+        <div className="appHeader">
+          <label > APPLICATION FORM </label> <br />
+        </div>
+        <div className="appAlign" >
+          <Grid >
             <Cell col={4}></Cell>
             <Cell className="appForm" col={4}>
-        
-            NAME     :<Textfield 
-            
-              onChange={() => { this.setState({ name:  }) }}
-              label="Text..."
-              style={{ width: '200px'}}
-            /><br/>
-            EMAIL ID :<Textfield
-              onChange={() => { }}
-              label="Text..."
-              style={{ width: '200px' }}
-            /><br/> 
-            PHONE NO :<Textfield
-              onChange={() => { }}
-              pattern="-?[0-9]*(\.[0-9]+)?"
-              error="Input is not a number!"
-              label="Number..."
-              style={{ width: '200px' }}
-            /><br/>
-            AADHAR NO:<Textfield
-              onChange={() => { }}
-              pattern="-?[0-9]*(\.[0-9]+)?"
-              error="Input is not a number!"
-              label="Number..."
-              style={{ width: '200px' }}
-            /><br/>
-            PAN NO   :<Textfield
-              onChange={() => { }}
-              pattern="-?[0-9]*(\.[0-9]+)?"
-              error="Input is not a number!"
-              label="Number..."
-              style={{ width: '200px' }}
-            /><br/>
-            
-            <div className="appButton"><Button onClick={() => this.page()} raised colored class="appButton">Submit</Button>
-            </div>
-        </Cell>
-        <Cell col={4}></Cell>
-       </Grid> </div>
+
+              <Textfield
+                onChange={(valName) => { this.setState({ name: valName.target.value }) } }
+                label="Name"
+                floatingLabel
+                pattern="-?[a-z]*(\.[A-Z]+)?"
+                error="Name should be Alphabetic!"
+                style={{ width: '280px' }}
+                value={this.state.name}
+                /><br />
+              <Textfield
+                onChange={(valEmail) => { this.setState({ email: valEmail.target.value }) } }
+                label="Email"
+                floatingLabel
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                error="Email should contain '@' and '.' symbol"
+                style={{ width: '280px' }}
+                value={this.state.email}
+                /><br />
+              <Textfield
+                onChange={(valPhone) => { this.setState({ phone: valPhone.target.value }) } }
+                label="Phone Number"
+                floatingLabel
+                //pattern="-?[0-9]*(\.[0-9]+)?"
+                //error="Input is not a number!"
+                pattern="(^\d{10}$)"  
+                error="Phone Numbr must be 10 digit!"
+                style={{ width: '280px' }}
+                value={this.state.phone}
+                /><br />
+              <Textfield
+                onChange={(valAadhar) => { this.setState({ aadhar: valAadhar.target.value }) } }
+                label="Aadhar Number"
+                floatingLabel
+                pattern="^\d{12}$"  
+                error="Aadhar Numbr must be 12 digit!"
+                // pattern="-?[0-9]*(\.[0-9]+)?"
+                // error="Input is not a number!"
+                style={{ width: '280px' }}
+                value={this.state.aadhar}
+                /><br />
+              <Textfield
+                onChange={(valPan) => { this.setState({ pan: valPan.target.value }) } }
+                label="Pan Number"
+                floatingLabel
+                pattern="^\d{10}$"  
+                error="Pan Numbr must be 10 digit!"
+                // pattern="-?[0-9]*(\.[0-9]+)?"
+                // error="Input is not a number!"
+                style={{ width: '280px' }}
+                value={this.state.pan}
+                /><br />
+
+              <div className="appButton"><Button onClick={() => this.page()} raised colored class="appButton">Submit</Button>
+              </div>
+            </Cell>
+            <Cell col={4}></Cell>
+          </Grid> </div>
       </div>
-   
+
     );
   }
 }
 
 export default App;
+
+
+
--- Header.js
+++ Header.js
@@ -1,40 +1,76 @@
 import React, { Component } from 'react';
 import './Header.css';
+import { Button, Textfield, Grid, Cell } from 'react-mdl';
 //import { Link } from 'react-router'
 
 class Header extends Component {
 
     constructor(props) {
-      super(props);
-       this.state = {
-        data : this.props.userData
-     };
-  
-   }
-    componentWillMount(){
+        super(props);
+        this.state = {
+            data: this.props.userData
+        };
+
+    }
+    componentWillMount() {
         console.log(this.state.data);
-        
+
     }
 
     render() {
         return (
-            <div>
-                <h2>Registration Successful</h2>
-                  
-                    <pre>
-                        <h3>
-                            Name : {this.state.data.name}  <br /><br />
-                            Mail Id:{this.state.data.email} <br /><br />
-                            Phone Number: {this.state.data.phone} <br /><br />
-                            Aadhar Number: {this.state.data.aadhar} <br /><br />
-                            Pan Number: {this.state.data.pan} <br /><br />
-                        </h3>
-                    </pre>
-               
+            <div className="headerBody" >
+
+                <div className="headerTab">
+                    <label > Registration Successful</label> <br />
+                </div>
+
+                <Grid >
+                    <Cell col={4}></Cell>
+                    <Cell className="appForm" col={4}>
+
+                        <div className="tableBody">
+
+                            <tbody>
+                                <h5>
+                                    <tr>
+                                        <td className="mdl-data-table__cell--non-numeric">Name</td>
+                                        <td>{this.state.data.name}</td>
+
+
+                                    </tr>
+                                    <tr>
+                                        <td className="mdl-data-table__cell--non-numeric">Emailid</td>
+                                        <td>{this.state.data.email}</td>
+
+                                    </tr>
+                                    <tr>
+                                        <td className="mdl-data-table__cell--non-numeric">Phone No:</td>
+                                        <td>{this.state.data.phone}</td>
+
+                                    </tr>
+                                    <tr>
+                                        <td className="mdl-data-table__cell--non-numeric">Aadhar No:</td>
+                                        <td>{this.state.data.aadhar}</td>
+
+                                    </tr>
+                                    <tr>
+                                        <td className="mdl-data-table__cell--non-numeric">Pan No:</td>
+                                        <td>{this.state.data.pan}</td>
+
+                                    </tr>
+                                </h5>
+                            </tbody>
+
+                        </div>
+
+                    </Cell>
+                    <Cell col={4}></Cell>
+                </Grid>
+
             </div>
-        )
+        );
     }
 }
-
 
 export default Header;
